b3a3fd1a27e0bfbb0980443fd4c7926c2c74f80e
mintaeho <mintaeho75@gmail.com>
2021-07-04 02:16:34 +0900

    무중단 배포 기능 추가

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,6 +11,9 @@ function switch_proxy() {
     echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-    echo "> 엔진엑스 Reload"
-    sudo service nginx reload
+    #echo "> 엔진엑스 Reload"
+    #sudo service nginx reload
+
+    echo "> 배포된 App을 확인한 후 이상이 없는 경우 엔진엑스를 Reload 하세요."
+    echo "> sudo service nginx reload"
 }
